befb2ce3a970dcbddb03962ac6bf38be1c184e32
gabrielmg7 <gabrielmouraguimaraes@gmail.com>
2023-06-01 14:38:38 -0300

    criando request dos Campus

--- src/api/utils/get/getAllCampus.tsx
+++ src/api/utils/get/getAllCampus.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { CampusType } from '../../types/CampusType';
 
-export const getPolos = () => {
-  return (
-    <div>getPolos</div>
-  )
+import { app } from '../../config/firebaseConfig';
+import { collection, getDocs, getFirestore } from 'firebase/firestore';
+
+
+//Método para retornar os campus cadastrados
+export async function getPolos() {
+  const db = getFirestore(app);
+  try {
+    const querySnapshot = await getDocs(collection(db, "produtos"));
+    let unidades: CampusType[] = [];
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      const campus: CampusType = {
+        nome: data.nome,
+        rua: data.rua,
+        numero: data.numero,
+        bairo: data.bairro
+      }
+      unidades.push(campus);
+    });
+    return unidades;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 }
