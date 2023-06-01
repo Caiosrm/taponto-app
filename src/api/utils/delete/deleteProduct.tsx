c07db2b932b9766ea87ac5eadc9135fde0629063
gabrielmg7 <gabrielmouraguimaraes@gmail.com>
2023-06-01 17:01:46 -0300

    criando delete requests

--- src/api/utils/delete/deleteProduct.tsx
+++ src/api/utils/delete/deleteProduct.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import { app } from '../../config/firebaseConfig';
+
+import { getFirestore, collection, doc, deleteDoc } from '@firebase/firestore';
+
 
 //Método que exclui um documento na coleção do firebase
-export const deleteProduct = () => {
+export async function deleteProduto(produtoId: string) {
+    const db = getFirestore(app);
+    const produtosRef = collection(db, "produtos");
+    const produtoDocRef = doc(produtosRef, produtoId);
 
+    try {
+        await deleteDoc(produtoDocRef);
+        console.log("Produto excluído com sucesso!");
+    } catch (error) {
+        console.log("Erro ao excluir o produto:", error);
+    }
 }
+
