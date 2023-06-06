d25a457b19702657512a3a4c80261cad293d73fe
gabrielmg7 <gabrielmouraguimaraes@gmail.com>
2023-06-06 12:59:54 -0300

    incluindo logo

--- src/api/types/ShopType.ts
+++ src/api/types/ShopType.ts
@@ -1,17 +1,17 @@
-import { IProduto } from "../../components/Screen/Cardapio/types";
 import { PedidoType } from "./OrderType";
+import { ProdutoType } from "./ProductType";
 
 export type CantinaType = {
- id: string;
- idCampus: string;
- nome: string;
- descricao: string;
- status: string;
- cardapio: IProduto[];
- avaliacoes: {
+  id: string;
+  idCampus: string;
+  nome: string;
+  descricao: string;
+  status: string;
+  cardapio: ProdutoType[];
+  avaliacoes: {
     comentario: string;
     clienteId: string;
     nota: number;
- }
- pedidos: PedidoType[];
-}
+  };
+  pedidos: PedidoType[];
+};
