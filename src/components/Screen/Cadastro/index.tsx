e221f78319ab235a34b4e71dadf2fd90ed576a1c
Alissondiasdev <alissondias120894@gmail.com>
2023-06-08 18:53:16 -0300

    recriando tela de cadastro

--- src/components/Screen/Cadastro/index.tsx
+++ src/components/Screen/Cadastro/index.tsx
@@ -1,16 +1,23 @@
-import { Box, FormControl, Input, Button } from "native-base";
+import { Box, FormControl, Input, Button, VStack, Text, Icon, Image } from "native-base";
 import React, { useState } from "react";
 import { ICadastroScreenProps } from "./types";
+import { useNavigation } from '@react-navigation/native';
+import Logo from '../../../../assets/logo/logo2.png'
 import { auth } from "../../../api/config/firebaseConfig";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { TouchableOpacity } from "react-native";
+
 
 const CadastroScreen = (props: ICadastroScreenProps) => {
+    const navigation = useNavigation();
 
     const [email, setEmail] = React.useState<string>('')
     const [senha, setSenha] = React.useState<string>('')
     const [confirmarSenha, setconfirmarSenha] = React.useState<string>('')
 
     async function realizarCadastro() {
+        
+      
         await createUserWithEmailAndPassword(auth, email, senha)
             .then((dadosdousuario) => {
                 console.log(dadosdousuario)
@@ -24,57 +31,83 @@ const CadastroScreen = (props: ICadastroScreenProps) => {
         setconfirmarSenha('')
         alert('Usuário cadastrado com sucesso')
     }
+    const handleLogin = () => {
+        navigation.navigate('Login');
+      };
 
 
     return (
-        <Box padding={5}>
-            <FormControl>
-                <FormControl.Label>
-                    Email
-                </FormControl.Label>
+            
+        <VStack flex={1} alignItems='center' p={5}>
+            <Image mt={5} size='xl' source={Logo} alt='logo tá pronto' />
+        
+        <Text fontSize='2xl'
+            fontWeight='bold'
+            textAlign='center'
+            mt={5}
+        >Faça seu cadastro para continuar</Text>
+        <Box>
+            <FormControl mt={3}>
+                <FormControl.Label>Nome Completo</FormControl.Label>
                 <Input
-                    placeholder='Insira o email do estudante'
-                    size="lg"
-                    w="100%"
-                    value={email}
-                    onChangeText={texto => setEmail(texto)}
-                    borderRadius="lg"
-                    bgColor="gray.100"
-                    shadow={3} />
+                    borderRadius='lg'
+                    borderColor='primary.900'
+
+                    w={{ base: "100%" }}
+                    
+                    placeholder="Digite seu nome completo" />
             </FormControl>
 
-            <FormControl>
-                <FormControl.Label>
-                    Senha
-                </FormControl.Label>
-                <Input placeholder='Crie sua senha com 6 caracteres'
-                    type="password"
-                    size="lg"
-                    w="100%"
-                    value={senha}
-                    onChangeText={texto => setSenha(texto)}
-                    borderRadius="lg"
-                    bgColor="gray.100"
-                    shadow={3} />
-            </FormControl>
-
-            <FormControl>
-                <FormControl.Label>
-                    Confirmar senha
-                </FormControl.Label>
+            <FormControl mt={3} >
+            <FormControl.Label>Celular</FormControl.Label>
                 <Input
-                    placeholder='Confirme sua senha'
-                    size="lg"
-                    w="100%"
-                    value={confirmarSenha}
-                    onChangeText={texto => setconfirmarSenha(texto)}
-                    borderRadius="lg"
-                    bgColor="gray.100"
-                    shadow={3} />
+                    borderRadius='lg'
+                    borderColor='primary.900'
+                    w={{ base: "100%" }}                   
+                    placeholder="Digite com ddd" />
             </FormControl>
 
-            <Button borderRadius={'lg'} mt={10} onPress={() => realizarCadastro()}>Cadastrar</Button>
+            <FormControl mt={3}>
+            <FormControl.Label>Email</FormControl.Label>
+                    <Input
+                        borderRadius='lg'
+                        borderColor='primary.900'
+                        w={{ base: "100%" }}
+                        placeholder="Preencha com seu email" />
+                </FormControl>
+
+                <FormControl mt={3} >
+                <FormControl.Label>Senha</FormControl.Label>
+                    <Input
+                        borderRadius='lg'
+                        borderColor='primary.900'
+                        w={{ base: "100%" }}
+                        placeholder="Senha" />
+                </FormControl>
+
+                <FormControl mt={3} >
+                <FormControl.Label>Confirmar senha</FormControl.Label>
+                    <Input
+                        borderRadius='lg'
+                        borderColor='primary.900'
+                        w={{ base: "100%" }}
+                        placeholder="Confirme sua senha" />
+                </FormControl>
+
+            <Box mt={3}>
+                
+                <Button mt={5} bg='primary.900'>Cadastrar</Button>
+            </Box>
+
+
+            <Box justifyContent='center' alignItems='center' mt={5} flexDirection='row'>
+                <Text textAlign='center'>Já tem uma conta? </Text>
+                <TouchableOpacity onPress={handleLogin}><Text>Faça seu login</Text></TouchableOpacity>
+            </Box>             
+
         </Box>
+    </VStack>
+       
     );
 };
 
--- src/routes/AppNavigation.tsx
+++ src/routes/AppNavigation.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
+
 import Perfil from '../components/Screen/Perfil';
 import Login from '../components/Screen/Login';
 import ListarCardapio from '../components/Screen/Cantina/ListarCardapio';
@@ -9,6 +10,7 @@ import ItemDetalhado from '../components/Screen/Cantina/ItemDetalhado';
 import CantinaScreen from '../components/Screen/Cantina';
 import HomeTabsNavigator from './utils/HomeTabsNavigator';
 import BottomTabNavigator from './utils/BottomTabNavigator';
+import CadastroScreen from '../components/Screen/Cadastro';
 
 const Stack = createStackNavigator();
 
@@ -17,7 +19,7 @@ const Stack = createStackNavigator();
 /*===================================================================================================*/
 export const StackNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName='Login'>
+    <Stack.Navigator initialRouteName='Cadastro'>
       <Stack.Screen name="TabNavigator" component={BottomTabNavigator} options={{ headerShown: false }} />
       <Stack.Screen name="Home" component={HomeTabsNavigator} options={{ headerShown: false }} />
       <Stack.Screen name="ListarCardapio" component={ListarCardapio} options={{ headerShown: false }} />
@@ -26,6 +28,8 @@ export const StackNavigator = () => {
       <Stack.Screen name="Perfil" component={Perfil} options={{ headerShown: false }} />
       <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
       <Stack.Screen name="CantinaScreen" component={CantinaScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Cadastro" component={CadastroScreen} options={{ headerShown: false }} />
+      
     </Stack.Navigator>
   )
 }
