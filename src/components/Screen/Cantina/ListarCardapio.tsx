b687b134e13204fb0de1ccea8baa13989a0389fe
Alissondiasdev <alissondias120894@gmail.com>
2023-06-12 17:31:44 -0300

    modificando tela listacardapio

--- src/components/Screen/Cantina/ListarCardapio.tsx
+++ src/components/Screen/Cantina/ListarCardapio.tsx
@@ -2,7 +2,7 @@ import { Text, HStack, Box, Avatar, VStack, Spacer, Button, Image, useTheme, Ico
 import React, { useEffect } from "react";
 
 import { getFirestore } from '@firebase/firestore';
-import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+import { AntDesign, Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { FlatList, TouchableOpacity } from "react-native";
 import { colors } from "../../../themes/Theme";
 import TopBar from "../../Common/TopBar";
@@ -11,6 +11,7 @@ import { getCardapio } from "../../../api/utils/get/getCardapio";
 import { CardapioType, initialStateCardapio } from "../../../api/types/CardapioType";
 import { HeaderCantina } from "../../Common/Header";
 import productFeaturedImage from '../../../../assets/product-featured.png';
+import { color } from "native-base/lib/typescript/theme/styled-system";
 
 
 interface ICardapioProps {
@@ -38,57 +39,57 @@ const ListarCardapio: React.FC = ({ navigation, route }: any) => {
 
     return (
         <View>
+            <Box>
             <HeaderCantina pageTitle={cantinaId} />
+           
+
+            </Box>
+            
+            
+
+            
             <Box justifyContent='center' alignItems='center' padding={7}>
                 <Text fontSize='26'>Cardápio</Text>
                 <Text textAlign='center'>Onde você encontra todos os produtos, ordenados ou filtrados</Text>
                 <Input placeholder="Pesquise pelo nome " mt={5} w='100%' />
             </Box>
+           
             <FlatList
                 showsHorizontalScrollIndicator={true}
                 keyExtractor={(item) => String(item)}
                 pagingEnabled
                 data={cardapio?.itens}
-                numColumns={2}
+
                 renderItem={({ item }) => (
-                    <Box padding={4} flex={1} flexDirection='column' mt={2} justifyContent='center'>
-                        <Box marginBottom={2} alignItems="center">
-                            <Box maxW="80" rounded="lg" overflow="hidden" borderColor="coolGray.200" borderWidth="1" _dark={{
-                                borderColor: "coolGray.600",
-                                backgroundColor: "gray.700"
-                            }} _web={{
-                                shadow: 2,
-                                borderWidth: 0
-                            }} _light={{
-                                backgroundColor: "gray.50"
-                            }}>
-                                <Box>
-                                    <AspectRatio w="100%" ratio={16 / 9}>
-                                        <Image source={{
-                                            uri: "https://i2.wp.com/files.agro20.com.br/uploads/2020/03/comidabrasileira3.jpg?resize=600%2C338&ssl=1"
-                                        }} alt="image" />
-                                    </AspectRatio>
+                    <Box padding={2}>
+                        <Box alignItems='center' borderWidth={1} borderColor={colors.light.brancoPuro} justifyContent='space-around' flex={1} flexDirection='row' mt={2} >
+                            <Box justifyContent='center' h='150px' w='60%'>
+                                <Text color={colors.light.azulTurquesa}>{item.nome}</Text>
+                                <Text>{item.descricao}</Text>
+                                <Text>R${item.valor}</Text>
+                                <Box mt={2}>
+                                    <TouchableOpacity>
+
+                                <Ionicons name="add-circle" size={24} color={colors.light.azulTurquesa} />
+                                    </TouchableOpacity>
+                               
+
                                 </Box>
-                                <Stack p="4" space={3}>
-                                    <Stack space={2}>
-                                        <Heading size="md" ml="-1">
-                                            {item.nome + " " + item.tipo}
-                                        </Heading>
-                                        <Text fontSize="xs" _light={{
-                                            color: "violet.500"
-                                        }} _dark={{
-                                            color: "violet.400"
-                                        }} fontWeight="500" ml="-0.5" mt="-1">
-                                            The Silicon Valley of India.
-                                        </Text>
-                                    </Stack>
-                                </Stack>
+                            </Box>
+                            <Box>
+                                
+                                 <Text>imagem</Text>
+                                
                             </Box>
                         </Box>
                     </Box>
                 )}
-            />
+                />
 
+           
+
+                
+                
 
 
 
